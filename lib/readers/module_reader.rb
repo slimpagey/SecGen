9a2d381e9ad560669957ca43d75cd6a65a32fed3
Z. Cliffe Schreuders <code.z.cliffe@schreuders.org>
2016-11-08 00:15:04 +0000

    default values -- complete

--- lib/readers/module_reader.rb
+++ lib/readers/module_reader.rb
@@ -142,59 +142,38 @@ class ModuleReader
 
       # for each default input
       doc.xpath("/#{module_type}/default_input").each do |inputs_doc|
-        Print.std "inputs_doc = #{inputs_doc.to_s}"
         inputs_doc.xpath('descendant::vulnerability | descendant::service | descendant::utility | descendant::network | descendant::base | descendant::encoder | descendant::generator').each do |module_node|
-          Print.err "module_node = #{module_node.to_s}"
 
           # create a selector module, which is a regular module instance used as a placeholder for matching requirements
           module_selector = Module.new(module_node.name)
 
           # create a unique id for tracking variables between modules
           module_selector.unique_id = "#{module_node.path}#{module_path}".gsub(/[^a-zA-Z0-9]/, '')
-          Print.debug "module_selector.unique_id = #{module_selector.unique_id}"
           # check if we need to be sending the module output to another module
           module_node.xpath('parent::input').each do |input|
             # Parent is input -- track that we need to send write value somewhere
             input.xpath('..').each do |input_parent|
               module_selector.write_output_variable = input.xpath('@into').to_s
               module_selector.write_to_module_with_id = "#{input_parent.path}#{module_path}".gsub(/[^a-zA-Z0-9]/, '')
-              Print.debug "+module_selector.write_output_variable: #{module_selector.write_output_variable}"
-              Print.debug "+module_selector.write_to_module_with_id = #{module_selector.write_to_module_with_id}"
             end
           end
-          # TODO FIXME GENERATORS NOT COMING THROUGH????????????????????????
-          Print.err "module_node.xpath('parent::default_input'): #{module_node.xpath('parent::default_input')}"
           if module_node.xpath('parent::default_input').to_s != ''
             # input for this module -- track that we need to send write value to the module itself
             module_selector.write_output_variable = module_node.xpath('parent::default_input/@into').to_s
 
             module_selector.write_to_module_with_id = 'vulnerabilitydefaultinput'
-
-            Print.debug "-module_selector.write_output_variable: #{module_selector.write_output_variable}"
-            Print.debug "-module_selector.write_to_module_with_id = #{module_selector.write_to_module_with_id}"
-          else
-            Print.debug "-SKIPPPNG"
-
           end
 
           # check if we are being passed an input *literal value*
-          Print.verbose "  -- m start"
           module_node.xpath('input/value').each do |input_value|
             variable = input_value.xpath('../@into').to_s
             value = input_value.text
-            Print.verbose "  -- m literal value: #{variable} = #{value}"
             (module_selector.default_inputs_literals[variable] ||= []).push(value)
           end
-          Print.verbose "  -- m end"
-
 
           into = module_node.xpath('ancestor::default_input/@into').to_s
-          Print.debug "Adding default module: #{into} #{module_selector.inspect}"
-
-          # SystemReader.insert_selector_into_list(module_selector, (new_module.default_inputs_selectors["#{into}"] ||= []))
 
           (new_module.default_inputs_selectors["#{into}"] ||= []).unshift(module_selector)
-
 
           module_node.xpath('@*').each do |attr|
             module_selector.attributes["#{attr.name}"] = [attr.text] unless attr.text.nil? || attr.text == ''
@@ -205,30 +184,19 @@ class ModuleReader
               Print.verbose "  - #{attr[0].to_s} ~= #{attr[1].to_s}"
             end
           end
-
-
         end
 
         # check if we are being passed an input *literal value* -- to the containing module's default_value itself (as opposed to a module selector)
         inputs_doc.xpath('value').each do |input_value|
-
           variable = input_value.xpath('parent::default_input/@into').to_s
           value = input_value.text
-          Print.verbose "  -- d literal value: #{variable} = #{value}"
 
           (new_module.default_inputs_literals[variable] ||= []).push(value)
-
         end
-
-        Print.debug "new_module.default_inputs_literals: #{new_module.default_inputs_literals}"
-        # Print.debug "new_module.default_inputs_selectors = #{new_module.default_inputs_selectors}"
-
       end
 
       modules.push(new_module)
-
     end
-    Print.debug "modules: #{modules.inspect}"
 
     return modules
   end
